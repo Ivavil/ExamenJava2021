44fa0870dd92511686a65f3f2c75618d2c06eea1
ivand <ivandiscobolo@gmail.com>
2021-10-08 15:50:53 +0200

    Clase MedidaC02: atributos, constructor y getters y setters (corrección tipo variables)

--- CalidadAire/src/main/java/com/example/calidadaire/CalidadAire.java
+++ CalidadAire/src/main/java/com/example/calidadaire/CalidadAire.java
@@ -1,5 +1,8 @@
 package com.example.calidadaire;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalidadAire {
     // Hora local Gandia Epoch del 7.10.2021 de las 8:00 am a las 17:00 pm
     static long FECHA[]={1633586400000L, 1633590000000L, 1633593600000L, 1633597200000L, 1633600800000L, 1633604400000L, 1633608000000L,1633611600000L, 1633615200000L, 1633618800000L};
@@ -8,4 +11,8 @@ public class CalidadAire {
     static int LELECTRONICA[] = {500, 650, 700, 850, 875, 600, 550, 650, 850, 600};
     static int LTELEMATICA[] = {450, 500, 675, 875, 850, 700, 750, 880, 700, 600};
     static int AULA9[] = {450, 500, 550, 650, 700, 800, 750, 600, 650, 600};
+
+    List<MedidaC02> lista_medidas = new ArrayList<MedidaC02>();
+
+    for(int i = 0; i < LELEC; i++)
 }
--- CalidadAire/src/main/java/com/example/calidadaire/MedidaC02.java
+++ CalidadAire/src/main/java/com/example/calidadaire/MedidaC02.java
@@ -1,47 +1,47 @@
 package com.example.calidadaire;
 
 public class MedidaC02 {
-    private int t_mili;
-    private double conc_elec;
-    private double conc_telem;
-    private double conc_a9;
+    private long t_mili;
+    private int conc_elec;
+    private int conc_telem;
+    private int conc_a9;
 
-    public MedidaC02(int t_mili, double conc_elec, double conc_telem, double conc_a9) {
+    public MedidaC02(long t_mili, int conc_elec, int conc_telem, int conc_a9) {
         this.t_mili = t_mili;
         this.conc_elec = conc_elec;
         this.conc_telem = conc_telem;
         this.conc_a9 = conc_a9;
     }
 
-    public int getT_mili() {
+    public long getT_mili() {
         return t_mili;
     }
 
-    public void setT_mili(int t_mili) {
+    public void setT_mili(long t_mili) {
         this.t_mili = t_mili;
     }
 
-    public double getConc_elec() {
+    public int getConc_elec() {
         return conc_elec;
     }
 
-    public void setConc_elec(double conc_elec) {
+    public void setConc_elec(int conc_elec) {
         this.conc_elec = conc_elec;
     }
 
-    public double getConc_telem() {
+    public int getConc_telem() {
         return conc_telem;
     }
 
-    public void setConc_telem(double conc_telem) {
+    public void setConc_telem(int conc_telem) {
         this.conc_telem = conc_telem;
     }
 
-    public double getConc_a9() {
+    public int getConc_a9() {
         return conc_a9;
     }
 
-    public void setConc_a9(double conc_a9) {
+    public void setConc_a9(int conc_a9) {
         this.conc_a9 = conc_a9;
     }
 }
